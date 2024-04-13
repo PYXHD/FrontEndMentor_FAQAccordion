c88c3cd8381ad9d63c258c94cbe0b5855af40ab9
PYXHD <marc-charvot@hotmail.fr>
2024-04-13 21:19:19 +0200

    Commit

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,6 +3,10 @@ const answers = document.querySelectorAll(".answer");
 const plusIcon = document.querySelectorAll(".plus-icon");
 const minusIcon = document.querySelectorAll(".minus-icon");
 
+answers[0].style.display = "block";
+plusIcon[0].style.visibility = "hidden";
+minusIcon[0].style.visibility = "visible";
+
 questions.forEach((question, index) => {
     question.addEventListener("click", () => {
 
